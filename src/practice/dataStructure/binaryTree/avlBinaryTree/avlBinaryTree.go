64faee304c62fd6fac83bc92268c93ec7031d927
suhanyujie <suhanyujie@qq.com>
2018-09-12 09:16:56 +0800

    实现avl树相关的方法

--- src/practice/dataStructure/binaryTree/avlBinaryTree/avlBinaryTree.go
+++ src/practice/dataStructure/binaryTree/avlBinaryTree/avlBinaryTree.go
@@ -5,6 +5,15 @@ package avlBinaryTree
 	介绍+实现  https://www.cnblogs.com/skywang12345/p/3576969.html
 	一共有4种旋转操作
 
+实现步骤：
+	1.方法1：创建树				100%
+	2.方法2：获取树的高度			100%
+	3.方法3：比较两个值的大小		100%
+	4.方法4.1 左左旋
+	5.方法4.2 左右旋
+	6.方法4.3 右左旋
+	7.方法4.4 右右旋
+
  */
 
 type TreeNode struct {
@@ -20,6 +29,43 @@ type AvlTree struct {
 	Length int
 }
 
+//1.创建树
+func CreateAvlTree(data interface{},left *TreeNode,right *TreeNode) *TreeNode {
+	newNode := new(TreeNode)
+	newNode.Left = left
+	newNode.Right = right
+	newNode.Data = data
+	newNode.Height = 1
+
+	return newNode
+}
+
+//2.获取树的高度
+/**
+树的高度为最大层次。即空的二叉树的高度是0，非空树的高度等于它的最大层次(根的层次为1，根的子节点为第2层，依次类推)。
+ */
+func Height(tree *TreeNode) int {
+	if tree == nil {
+		return 0
+	}
+	return tree.Height
+}
+
+//3.比较2个interface{}类型的数据大小，返回大的那个值
+func MaxOfI(data1,data2 interface{}) interface{} {
+	num1 := data1.(int)
+	num2 := data2.(int)
+	if num1>num2 {
+		return data1
+	} else {
+		return data2
+	}
+}
+
+//4.1 左旋
+/**
+
+ */
 func LLRotation(node *TreeNode) *TreeNode {
 	k1 := node.Left
 	node.Left = k1.Right
--- src/practice/dataStructure/binaryTree/main.go
+++ src/practice/dataStructure/binaryTree/main.go
@@ -9,6 +9,12 @@ import (
 
 // 入口函数
 func main() {
+
+
+
+
+
+	//二叉搜索树的初始化
 	bst := binarySearchTree.GetInitBst()
 	for i := 0; i < 24; i++ {
 		randVal := rand.Intn(300)
@@ -17,6 +23,7 @@ func main() {
 	fmt.Println("------------------")
 	bst.MiddleTraverse(bst.Root)
 	searchValue := 74
+	//二叉搜索树的搜索
 	findedNode := binarySearchTree.SearchNode(bst, searchValue)
 	if findedNode == nil {
 		fmt.Printf("\n没有找到值为%d的节点\n", searchValue)

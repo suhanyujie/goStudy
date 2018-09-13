e0040bb55a4c894ef57cdf0fb767fea9db2afa19
suhanyujie <suhanyujie@qq.com>
2018-09-13 21:38:17 +0800

    尝试实现 插入排序

--- src/practice/studySort/insertionSort/main.go
+++ src/practice/studySort/insertionSort/main.go
@@ -1,1 +1,21 @@
-package insertionSort
+package main
+
+/**
+插入排序：
+
+ */
+
+// 入口函数
+func main() {
+	var numArr = [10]int{21, 32, 19, 56, 29, 37, 16, 91, 126, 69}
+	numLength := len(numArr)
+	var newNumArr [...]int
+	for i := 0; i < numLength; i++ {
+		for j := i + 1; j < numLength; j++ {
+
+		}
+		if numArr[i] > numArr[i+1] {
+			numArr[i], numArr[i+1] = numArr[i+1], numArr[i]
+		}
+	}
+}

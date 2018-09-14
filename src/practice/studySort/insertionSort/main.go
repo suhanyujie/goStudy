ae1aef9d658202f11656bcb666a67aee9a3c015e
suhanyujie <suhanyujie@qq.com>
2018-09-14 08:33:46 +0800

    插入排序的实现

--- src/practice/studySort/insertionSort/main.go
+++ src/practice/studySort/insertionSort/main.go
@@ -1,21 +1,25 @@
 package main
 
+import "fmt"
+
 /**
 插入排序：
-
+	这种排序方法 比冒泡、选择排序要稍微难理解一点
  */
 
 // 入口函数
 func main() {
 	var numArr = [10]int{21, 32, 19, 56, 29, 37, 16, 91, 126, 69}
 	numLength := len(numArr)
-	var newNumArr [...]int
-	for i := 0; i < numLength; i++ {
-		for j := i + 1; j < numLength; j++ {
-
+	var j,compNumber int;
+	for i := 1; i < numLength; i++ {
+		compNumber = numArr[i]
+		for j = i - 1; j >= 0 && compNumber < numArr[j];  {
+			numArr[j+1], numArr[j] = numArr[j], numArr[j+1]
+			j--
 		}
-		if numArr[i] > numArr[i+1] {
-			numArr[i], numArr[i+1] = numArr[i+1], numArr[i]
-		}
+		numArr[j+1] = compNumber
 	}
+
+	fmt.Println(numArr)
 }
